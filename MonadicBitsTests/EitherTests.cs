2062402879fd794c588719922fd0dce595dfb3c0
Andreas Pfohl <mail@apfohl.com>
2021-03-01 10:06:59 +0100

    Added LINQ extensions for monads (#2)
    - Select and SelectMany for maybe
    - Select and SelectMany for either

--- MonadicBitsTests/EitherTests.cs
+++ MonadicBitsTests/EitherTests.cs
@@ -145,5 +145,39 @@ namespace MonadicBitsTests
             const string value = "Test";
             value.Right<string, string>().ToMaybe().Match(s => Assert.AreEqual(value, s), Assert.Fail);
         }
+
+        [Test]
+        public static void Select_with_null_selector_throws_exception() =>
+            Assert.Throws<ArgumentNullException>(() =>
+                "Test".Right<string, string>().Select((Func<string, string>) null));
+
+        [Test]
+        public static void Select_from_maybe_with_value_returns_maybe_with_value()
+        {
+            const string input = "Test";
+            (from s in input.Right<string, string>() select s).Match(Assert.Fail, s => Assert.AreEqual(input, s));
+        }
+
+        [Test]
+        public static void SelectMany_with_null_collection_throws_exception() =>
+            Assert.Throws<ArgumentNullException>(() => "Test".Right<string, string>().SelectMany(
+                (Func<string, Either<string, string>>) null, (i, c) => $"{i}{c}"));
+
+        [Test]
+        public static void SelectMany_with_null_selector_throws_exception() =>
+            Assert.Throws<ArgumentNullException>(() =>
+                "Test".Right<string, string>()
+                    .SelectMany(s => s.Right<string, string>(), (Func<string, string, string>) null));
+
+        [Test]
+        public static void SelectMany_from_either_with_right_value_returns_either_with_right_value()
+        {
+            const int input = 42;
+            (
+                from s in "Test".Right<string, string>()
+                from i in input.Right<string, int>()
+                select i
+            ).Match(Assert.Fail, i => Assert.AreEqual(input, i));
+        }
     }
 }
